fbdbc525a82fe95552d47892115f34f2bfb4227b
Deyan Dyankov <deyandyankov23@yahoo.com>
2024-04-23 16:49:04 +0200

    3js

--- 3js/test.js
+++ 3js/test.js
@@ -2,62 +2,111 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// Three JS uses the renderer to allocate space on a web browser
 const renderer = new THREE.WebGLRenderer();
-
-renderer.shadowMap.enabled = true;
-
+renderer.shadowMap.enabled = true; // enable map shadowing
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
 
+// creating the scene
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+// creating the camera
 
-const orbit = new OrbitControls( camera, renderer.domElement );
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
-camera.position.set(20, 50, 70);
+// add camera controls
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+// add AxesHelper to see the 3 axes (5  = length of axes)
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(-10 ,30, 30);
 orbit.update();
 
+//create The Box
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+// create a plane
 const planeGeometry = new THREE.PlaneGeometry(30,30);
-const planeMaterial = new THREE.MeshStandardMaterial({color: 0xFFFFFF, side:THREE.DoubleSide});
+const planeMaterial = new THREE.MeshStandardMaterial({
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
-plane.receiveShadow = true;
+plane.receiveShadow = true; // enable plane to receive shadow
 
+// add a Grid Helper
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-const sphereGeometry = new THREE.SphereGeometry(5, 50, 50);
-const sphereMaterial = new THREE.MeshStandardMaterial({color: 0x00FF0F, wireframe: false});
+// create a Sphere
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0x0000FF,
+    wireframe: false
+});
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
-
-sphere.position.set(10, -5, 0);
+sphere.position.set(-10, 10, 0);
 sphere.castShadow = true;
 
+// create Ambient Light
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
-scene.add(directionalLight);
-directionalLight.position.set(30,20,10);
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.top = 12;
+// create Directional Light
+// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+// scene.add(directionalLight);
+// directionalLight.position.set(-30, 50, 0);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.bottom = -12;
 
-const dLightHelper = new THREE.DirectionalLightHelper(directionalLight,5);
-scene.add(dLightHelper);
+// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+// scene.add(dLightHelper);
 
-const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-scene.add(dLightShadowHelper);
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(dLightShadowHelper);
 
+const spotLight = new THREE.SpotLight(0xFFFFFF, 100000);
+scene.add(spotLight);
+spotLight.position.set(-100, 100, 0);
+spotLight.castShadow = true;
+spotLight.angle = 0.2;
+
+const sLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
+
+// create fog
+//scene.fog = new THREE.Fog(0xFFFFFF, 0, 200);
+scene.fog = new THREE.FogExp2(0xFFFFFF, 0.01);
+
+//change background
+//renderer.setClearColor(0xFFEA00);
+const textureLoader = new THREE.TextureLoader().load('/westworld.jpg');
+scene.background = textureLoader;
+
+// use the GUI
 const gui = new dat.GUI();
 
-const options =  {
-    sphereColor: '#00ff0f',
+const options = {
+    sphereColor: '#ffea00',
     wireframe: false,
-    speed: 0.05
+    speed: 0.01,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 100000
 };
 
 gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -68,15 +117,27 @@ gui.add(options, 'wireframe').onChange(function(e){
     sphere.material.wireframe = e;
 });
 
-gui.add(options, 'speed', 0.05, 0.1);
+gui.add(options, 'speed', 0, 0.1);
+gui.add(options, 'angle', 0, 1);
+gui.add(options, 'penumbra', 0, 1);
+gui.add(options, 'intensity', 0, 100000);
+
 
 let step = 0;
 
-function animate()
-{
+function animate(time){
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step));
 
+    spotLight.angle = options.angle;
+    spotLight.penumbra = options.penumbra;
+    spotLight.intensity = options.intensity;
+    sLightHelper.update();
+
+    // link the scene and the camera with the renderer
     renderer.render(scene, camera);
 }
 
